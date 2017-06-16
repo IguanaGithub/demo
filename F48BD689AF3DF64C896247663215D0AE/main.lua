17cf3ff8146d13f06d6c189da946723e7a3aac87
admin <admin>
2017-06-16 19:14:09 +0000

    Export from Iguana Desktop: Commit

--- F48BD689AF3DF64C896247663215D0AE/main.lua
+++ F48BD689AF3DF64C896247663215D0AE/main.lua
@@ -6,7 +6,7 @@ DB = db.connect{api=db.SQL_SERVER, name='MSSQL', user='', password=''}
 function main(Data)
    
    -- Parse incoming raw message with hl7.parse
-   local MsgIn = hl7.parse{vmd='example/demo.vmd', data=Data}
+   local MsgIn, MsgName = hl7.parse{vmd='example/demo.vmd', data=Data}
    -- Build node tree of table to temporarily store information
    -- tables.vmd name = 'ADT'
    local TableOut = db.tables{vmd='tables.vmd', name='ADT'}
@@ -14,8 +14,11 @@ function main(Data)
    local queryResults = DB:query{sql='select * from Patient', live=true}
    
    -- Map fields from PID segment to Patient table
-   MapPatient(TableOut.Patient[1], MsgIn.PID)
-   
+   if MsgName == 'ORM' then 
+      MapORMPatient(TableOut.Patient[1], MsgIn.Group1.PID)
+   else
+      MapPatient(TableOut.Patient[1], MsgIn.PID)
+   end
    -- Insert information from TableOut into the real table
    DB:merge{data=TableOut, live=false}
    -- Change the above live=false to live=true 
@@ -30,6 +33,16 @@ function MapPatient(Patient, PID)
    Patient.Id        = PID[3][1][1]
    Patient.FirstName = PID[5][1][2]
    Patient.LastName  = PID[5][1][1][1]
+   Patient.Gender    = PID[8]   
+   -- Click the Patient Row to the right to see results
+   return Patient
+end
+
+function MapORMPatient(Patient, PID)
+   -- This function prepares the TableOut variable
+   Patient.Id        = PID[3][1]
+   Patient.FirstName = PID[5][2]
+   Patient.LastName  = PID[5][1][1]
    Patient.Gender    = PID[8]   
    -- Click the Patient Row to the right to see results
    return Patient
